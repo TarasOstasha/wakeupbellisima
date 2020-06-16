fc5c418e9678539e7faf7703a1c51a6a0fdf1fa9
TarasOstasha <tonyjoss1990@gmail.com>
2020-06-15 20:35:10 -0400

    add slider certificate

--- front/src/app/pages/about-us/about-us.component.ts
+++ front/src/app/pages/about-us/about-us.component.ts
@@ -7,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-us.component.less']
 })
 export class AboutUsComponent implements OnInit {
-  
-  constructor() {}
+  public slideWidth: any;
 
-  ngOnInit() {}
+  constructor() { }
+
+  ngOnInit() { }
 
   certificates = [
     { url: '../../../assets/imgs/certificate1.jpg' },
@@ -22,5 +23,48 @@ export class AboutUsComponent implements OnInit {
     { url: '../../../assets/imgs/certificate7.jpg' },
     { url: '../../../assets/imgs/certificate8.jpg' }
   ]
+
+  currentSlide = 0;
+  startSlider;
+  nextSlider = null;
+  endSlider;
+  public carousel: any;
+  public slide: any = 0;
+  nextSlide: any = 0;
+  centerSlideWidth: any;
+
+  counter: number = 1;
+
+  move(event) {
+    this.slideWidth = document.querySelector('.slides');
+    if (event.target.classList[2] == 'left') {
+      //console.log('left');
+      this.counter--;
+      const lastImg = this.certificates.length - 1;
+      //console.log(this.counter)
+      this.slide = this.slide + this.sliderWidth();
+      if (this.counter <= 0) { this.counter = lastImg + 1; return this.slide = -this.sliderWidth() * lastImg; }
+    }
+    if (event.target.classList[2] == 'right') {
+      //console.log('right');
+      this.counter++;
+      const lastImg = this.certificates.length - 1;
+      this.nextSlide = this.sliderWidth();
+      this.nextSlider = 1;
+      this.nextSlide = 0;
+      this.slide = this.slide - this.sliderWidth();
+      if (this.counter >= lastImg) {
+        //console.log(this.slide, 'if else move right', this.counter, 'if else move right')
+        this.counter = 1;
+        this.slide = 0;
+
+      }
+    }
+  }
+
+  // get dynamically width of <li> 
+  sliderWidth() {
+    return this.slideWidth.offsetWidth;
+  }
 
 }
