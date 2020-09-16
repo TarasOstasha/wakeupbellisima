6f37e31a53a2f1de2f1c3d7965414439f77528fe
TarasOstasha <tonyjoss1990@gmail.com>
2020-09-15 21:43:29 -0400

    add

--- back/app.js
+++ back/app.js
@@ -43,15 +43,16 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // redirect to HTTPS
-app.use(function(req, res, next) {
-  if ((req.get('X-Forwarded-Proto') !== 'https')) {
-    res.redirect('https://' + req.get('Host') + req.url);
-  } else
-    next();
-});
+// app.use(function(req, res, next) {
+//   if ((req.get('X-Forwarded-Proto') !== 'https')) {
+//     res.redirect('https://' + req.get('Host') + req.url);
+//   } else
+//     next();
+// });
 
 app.use(forceDomain({
   hostname: 'wakeupbellisima.com',
+  port: 80,
   protocol: 'https'
 }));
 
