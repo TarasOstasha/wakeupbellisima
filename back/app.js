ffd55b166268a8232d1768c32f9c5025b5e30419
TarasOstasha <tonyjoss1990@gmail.com>
2020-09-15 21:31:26 -0400

    to https

--- back/app.js
+++ back/app.js
@@ -1,6 +1,5 @@
 var createError = require('http-errors');
 var express = require('express');
-var secure = require('express-force-https'); // redirect to https
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -23,7 +22,6 @@ var usersRouter = require('./routes/users');
 var app = express();
 app.use(cors());
 app.use(bodyParser({ limit: '11111111mb' }));
-app.use(secure); // redirect to https
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -43,6 +41,13 @@ app.use(passport.session());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// redirect to HTTPS
+app.use(function(req, res, next) {
+  if ((req.get('X-Forwarded-Proto') !== 'https')) {
+    res.redirect('https://' + req.get('Host') + req.url);
+  } else
+    next();
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
