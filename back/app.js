664ba606f7052ecf1f8ba40d87df28d8f58fa70e
TarasOstasha <tonyjoss1990@gmail.com>
2020-09-15 20:48:44 -0400

    add secure redirect

--- back/app.js
+++ back/app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var secure = require('express-force-https'); // redirect to https
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -22,6 +23,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 app.use(cors());
 app.use(bodyParser({ limit: '11111111mb' }));
+app.use(secure); // redirect to https
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
